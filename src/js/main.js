2bb937a9dec4a6cb89696b8e6968c3c7013aef9a
Valentin <valkata033@abv.bg>
2022-02-27 20:19:20 +0200

    Create game loop

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,38 @@ const gameAreaElement = document.querySelector('.game-area');
 const gameScoreElement = document.querySelector('.game-score');
 const gameOverElement = document.querySelector('.game-over');
 
+const moveKeys = ['KeyA', 'KeyD', 'KeyW', 'KeyS', 'Space'];
+
 gameStartElement.addEventListener('click', (e) => {
     e.currentTarget.classList.add('hide');
+
     const wizardElement = document.createElement('div');
     wizardElement.classList.add('wizard');
     wizardElement.style.top = '200px';
     wizardElement.style.left = '200px';
     gameAreaElement.appendChild(wizardElement);
+
+    window.requestAnimationFrame(GameAction);
 });
+
+function GameAction() {
+    console.log('Hello')
+    window.requestAnimationFrame(GameAction);
+}
+
+addEventListener('keydown', (e) => {
+    if(moveKeys.includes(e.code)){
+        console.log(e.code);
+    }
+});
+
+addEventListener('keyup', (e) => {
+    if(moveKeys.includes(e.code)){
+        console.log(e.code);
+    }
+});
+
+
 
 
 
