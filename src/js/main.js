02bdae1a56a720536e5c33770eb843d833865cca
Valentin <valkata033@abv.bg>
2022-02-27 20:47:55 +0200

    Add movement to the wizard

--- src/js/main.js
+++ src/js/main.js
@@ -3,37 +3,66 @@ const gameAreaElement = document.querySelector('.game-area');
 const gameScoreElement = document.querySelector('.game-score');
 const gameOverElement = document.querySelector('.game-over');
 
-const moveKeys = ['KeyA', 'KeyD', 'KeyW', 'KeyS', 'Space'];
+let keys = {};
+
+let player = {
+    x: 150,
+    y: 100,
+};
+
+let game = {
+    speed: 2,
+    movingMultiplier: 4,
+};
+
+const availableKeys = ['KeyA', 'KeyD', 'KeyW', 'KeyS', 'Space'];
 
 gameStartElement.addEventListener('click', (e) => {
     e.currentTarget.classList.add('hide');
 
     const wizardElement = document.createElement('div');
     wizardElement.classList.add('wizard');
-    wizardElement.style.top = '200px';
-    wizardElement.style.left = '200px';
+    wizardElement.style.top = player.y + 'px';
+    wizardElement.style.left = player.x + 'px';
     gameAreaElement.appendChild(wizardElement);
 
     window.requestAnimationFrame(GameAction);
 });
 
 function GameAction() {
-    console.log('Hello')
+    const wizard = document.querySelector('.wizard');
+
+    if (keys.KeyW) {
+        player.y -= game.speed;
+    }
+    if (keys.KeyS) {
+        player.y += game.speed;
+    }
+    if (keys.KeyA) {
+        player.x -= game.speed;
+    }
+    if (keys.KeyD) {
+        player.x += game.speed;
+    }
+
+    wizard.style.top = player.y + 'px';
+    wizard.style.left = player.x + 'px';
+
     window.requestAnimationFrame(GameAction);
 }
 
-addEventListener('keydown', (e) => {
-    if(moveKeys.includes(e.code)){
-        console.log(e.code);
-    }
-});
+addEventListener('keydown', onKeyDown);
+addEventListener('keyup', onKeyUp);
 
-addEventListener('keyup', (e) => {
-    if(moveKeys.includes(e.code)){
-        console.log(e.code);
-    }
-});
+function onKeyDown(e) {
+    keys[e.code] = true;
+    console.log(keys);
+}
 
+function onKeyUp(e) {
+    keys[e.code] = false;
+    console.log(keys);
+}
 
 
 
